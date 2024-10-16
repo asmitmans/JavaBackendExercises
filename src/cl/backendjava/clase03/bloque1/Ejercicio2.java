d4693ebcd4d86b07427cc1648514a82b6fec0b98
asmitmans <a.smitmans@gmail.com>
2024-10-16 18:22:35 -0300

    clase3 ej1

---
+++ src/cl/backendjava/clase03/bloque1/Ejercicio2.java
@@ -0,0 +1,18 @@
+package cl.backendjava.clase03.bloque1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		Set<Integer> A = Set.of(1, 2, 3, 4, 5);
+		Set<Integer> B = Set.of(4, 5, 6, 7);
+		
+		Set<Integer> interseccion = new HashSet<>(A);
+		interseccion.retainAll(B);  
+		//interseccion.removeAll(B);  A - B
+		
+		System.out.println("Interseccion de A y B: " + interseccion);
+	}
+
+}
